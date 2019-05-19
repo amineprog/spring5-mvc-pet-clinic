481c268b25da14d04631ec6aa3de93b06e9157d2
Amine Rifi <2020.med.amine@gmail.com>
2019-05-19 04:31:20 +0100

    init Multi-Module Project for Data Model. closes #1

---
+++ pet-clinic-web/src/main/java/org/pet/clinic/web/Spring5MvcPetClinicApplication.java
@@ -0,0 +1,14 @@
+package org.pet.clinic.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5MvcPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5MvcPetClinicApplication.class, args);
+	}
+
+
+}
